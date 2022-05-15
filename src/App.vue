93f91d7a1ede04a3024f6a065b11fc342b2643c8
mingmoth <mingmoth@gmail.com>
2022-05-15 21:57:03 +0800

    feat: add Story component & basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,50 @@
 <template>
   <div id="app">
-    Hi Vue
+    <div class="slide">
+      <Story />
+      <Progress />
+    </div>
   </div>
 </template>
 
 <script>
+import Story from './components/Story.vue'
+import Progress from './components/Progress.vue'
 
 export default {
   name: 'App',
   components: {
+    Story,
+    Progress,
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+html, body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+}
+.slide {
+  margin: auto;
+  width: 100%;
+  height: 100%;
+  border: 1px solid red;
+  border-radius: 2rem;
+}
+
+@media screen and (min-width: 576px){
+  .slide {
+    width: 481px;
+    height: 809px;
+  }
 }
 </style>
